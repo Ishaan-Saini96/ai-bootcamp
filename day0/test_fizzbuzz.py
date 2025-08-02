e0bc87a404e9d85ebcc4dfc904c44443f347548c
Ishaan Saini <me.ishaansaini@gmail.com>
2025-08-02 14:35:55 +0530

    chore: ignore python cache files

---
+++ day0/test_fizzbuzz.py
@@ -0,0 +1,9 @@
+from fizzbuzz import fizzbuzz
+def test_fizzbuzz():
+    assert fizzbuzz(3) == "Fizz"
+    assert fizzbuzz(5) == "Buzz"
+    assert fizzbuzz(15) == "FizzBuzz"
+    assert fizzbuzz(2) == "2"
+
+print("All tests passed|")
+    
